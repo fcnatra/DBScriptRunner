94371350651dcade2da0130723ac59535e83f02b
frank.torres <frank.torres@ekasa.net>
2019-07-23 13:31:54 +0200

    WORKING ON: Scripts configuration - pending to test all UI operations

---
+++ DbScriptRunner/UI/frmAddEditScript.cs
@@ -0,0 +1,70 @@
+﻿using DbScriptRunnerLogic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DbScriptRunner.UI
+{
+    public partial class frmAddEditScript : Form
+    {
+        private Script _itemInformation;
+
+        public Script ItemInformation {
+            get
+            {
+                if (_itemInformation == null) _itemInformation = new Script();
+                return _itemInformation;
+            }
+
+            set
+            {
+                _itemInformation = value;
+                InitializeDatabaseInformationUI();
+            }
+        }
+
+        public delegate IEnumerable<string> Delegate_GetErrorsOnItemInformation(Script proposedDatabaseInfo);
+
+        public Delegate_GetErrorsOnItemInformation GetErrorsOnItemInformation { get; set; }
+
+        public frmAddEditScript()
+        {
+            InitializeComponent();
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void InitializeDatabaseInformationUI()
+        {
+            this.txtName.Text = ItemInformation.Name;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            var databaseInfo = new Script
+            {
+                Name = this.txtName.Text
+            };
+
+            if (GetErrorsOnItemInformation != null)
+            {
+                var errorList = GetErrorsOnItemInformation(databaseInfo);
+                if (errorList.Any())
+                {
+                    var message = string.Empty;
+                    foreach (var error in errorList) message += error + Environment.NewLine;
+                    MessageBox.Show(message, "INFORMATION");
+                    return;
+                }
+            }
+            this.ItemInformation = databaseInfo;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- DbScriptRunner/UI/frmMain.cs
+++ DbScriptRunner/UI/frmMain.cs
@@ -13,7 +13,8 @@ namespace DbScriptRunner.UI
 {
     public partial class frmMain : Form
     {
-        private AppData<Database> _databasesAppData;        
+        private AppData<Database> _databasesAppData;
+        private AppData<Script> _scriptsAppData;
 
         public frmMain()
         {
@@ -22,6 +23,7 @@ namespace DbScriptRunner.UI
             ConfigureLogicDependencies();
 
             _databasesAppData.RecoverLastStatus();
+            _scriptsAppData.RecoverLastStatus();
 
             InitializeControlsDesign();
         }
@@ -29,52 +31,37 @@ namespace DbScriptRunner.UI
         private void ConfigureLogicDependencies()
         {
             var fileRepository = new TextFileRepository();
+
             _databasesAppData = new AppData<Database>();
             _databasesAppData.Persistence.Repository = fileRepository;
+
+            _scriptsAppData = new AppData<Script>();
+            _scriptsAppData.Persistence.Repository = fileRepository;
         }
 
         private void InitializeControlsDesign()
+        {
+            InitializeUIForDatabasesListView();
+            PopulateListView(_databasesAppData.Instances, lvDatabases, null, _databasesAppData.RepositoryInformation?.Name);
+
+            InitializeUIForScriptsListView();
+            PopulateListView(_scriptsAppData.Instances, lvScripts, null, _scriptsAppData.RepositoryInformation?.Name);
+        }
+
+        private void InitializeUIForScriptsListView()
+        {
+            lvScripts.Columns.Add(new ColumnHeader { DisplayIndex = 0, Width = 70, Text = "Order", Name = "lvScriptsHeaderOrder" });
+            lvScripts.Columns.Add(new ColumnHeader { DisplayIndex = 1, Width = 500, Text = "Name", Name = "lvScriptsHeaderName" });
+            lvScripts.Columns[0].TextAlign = HorizontalAlignment.Center;
+            lvScripts.View = View.Details;
+        }
+
+        private void InitializeUIForDatabasesListView()
         {
             lvDatabases.Columns.Add(new ColumnHeader { DisplayIndex = 0, Width = 70, Text = "Order", Name = "lvDatabasesHeaderOrder" });
             lvDatabases.Columns.Add(new ColumnHeader { DisplayIndex = 1, Width = 300, Text = "Name", Name = "lvDatabasesHeaderName" });
             lvDatabases.Columns[0].TextAlign = HorizontalAlignment.Center;
             lvDatabases.View = View.Details;
-
-            PopulateDatabasesListView();
-        }
-
-        private void PopulateDatabasesListView(List<int> indicesToSelect = null)
-        {
-            lvDatabases.Items.Clear();
-
-            var datasource = _databasesAppData.Instances;
-            if (datasource == null || !datasource.Any()) return;
-
-            lvDatabases.SuspendLayout();
-            int index = 0;
-            foreach (var database in datasource)
-            {
-                ListViewItem lvItem = lvDatabases.Items.Add(index.ToString());
-                lvItem.Tag = database;
-                lvItem.SubItems.Add(database.Name);
-                ++index;
-            }
-
-            SelectIndicesOnListView(lvDatabases, indicesToSelect);
-
-            if (!string.IsNullOrEmpty(_databasesAppData.RepositoryInformation?.Name))
-                lblDatabasesTitle.Text = _databasesAppData.RepositoryInformation.Name.ToUpper();
-
-            lvDatabases.ResumeLayout();
-        }
-
-        private void SelectIndicesOnListView(ListView lvControl, List<int> indicesToSelect)
-        {
-            if (indicesToSelect == null) return;
-
-            var maxIndex = lvControl.Items.Count;
-            foreach (var index in indicesToSelect)
-                lvControl.SelectedIndices.Add(index);
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -89,110 +76,34 @@ namespace DbScriptRunner.UI
 
         private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
         {
-            DialogResult dialogResult = DialogResult.OK;
-            if (_databasesAppData.HaveChanged())
-            {
-                dialogResult = MessageBox.Show("Databases changed. Want to save the current list?", "CONTENT HAVE CHANGED", MessageBoxButtons.YesNoCancel);
-                if (dialogResult == DialogResult.Yes)
-                    SaveDatabases();
-            }
+            CheckDatabaseConfigIsSaved(e);
+            if (e.Cancel) return;
 
-            if (dialogResult == DialogResult.Cancel)
-                e.Cancel = true;
-            else
-                _databasesAppData.BackupCurrentStatus();
+            CheckScriptConfigIsSaved(e);
         }
-
         private void toolbarMoveUp_Click(object sender, EventArgs e)
         {
-            if (!IsThereAnyItemSelected(lvDatabases))
-            {
-                CommonDialogs.TellUserToSelectItemsInOrderToMove();
-                return;
-            }
-
-            var selectedIndices = lvDatabases.SelectedIndices.Cast<int>().ToList();
-            var indicesToSelect = ((ArrangeableList<INamed>)_databasesAppData.Instances).MoveItemsUpOnePosition(selectedIndices);
-            PopulateDatabasesListView(indicesToSelect);
+            MoveItemUp();
         }
 
         private void toolbarMoveDown_Click(object sender, EventArgs e)
         {
-            if (!IsThereAnyItemSelected(lvDatabases))
-            {
-                CommonDialogs.TellUserToSelectItemsInOrderToMove();
-                return;
-            }
-
-            var selectedIndices = lvDatabases.SelectedIndices.Cast<int>().ToList();
-            var indicesToSelect = ((ArrangeableList<INamed>)_databasesAppData.Instances).MoveItemsDownOnePosition(selectedIndices);
-            PopulateDatabasesListView(indicesToSelect);
+            MoveItemDown();
         }
 
         private void toolbarRemove_Click(object sender, EventArgs e)
         {
-            if (!IsThereAnyItemSelected(lvDatabases))
-            {
-                CommonDialogs.TellUserToSelectItemsInOrderToMove();
-                return;
-            }
-
-            var selectedIndices = lvDatabases.SelectedIndices.Cast<int>().ToList();
-            var indicesToSelect = ((ArrangeableList<INamed>)_databasesAppData.Instances).RemoveAt(selectedIndices);
-            PopulateDatabasesListView(indicesToSelect);
+            RemoveItem();
         }
 
         private void toolbarAddNewItem_Click(object sender, EventArgs e)
         {
-            AddNewDatabase();
+            AddNewItem();
         }
 
         private void toolbarEdit_Click(object sender, EventArgs e)
         {
-            EditSelectedDatabaseItem();
-        }
-
-        private void EditSelectedDatabaseItem()
-        {
-            if (lvDatabases.SelectedIndices.Count != 1)
-            {
-                CommonDialogs.TellUserToSelectJustOneItem();
-                return;
-            }
-
-            var editForm = new frmAddEditDatabase();
-            editForm.GetErrorsOnDatabaseInformation = _databasesAppData.CheckForErrorsOnName;
-            var databaseList = ((ArrangeableList<INamed>)_databasesAppData.Instances);
-
-            editForm.DatabaseInformation = (Database)databaseList[lvDatabases.SelectedIndices[0]];
-
-            var dialogResult = editForm.ShowDialog();
-            if (dialogResult == DialogResult.OK)
-            {
-                var selectedIndices = lvDatabases.SelectedIndices.Cast<int>().ToList();
-                databaseList.ReplaceAt(lvDatabases.SelectedIndices[0], editForm.DatabaseInformation);
-                PopulateDatabasesListView(selectedIndices);
-            }
-        }
-
-        private void AddNewDatabase()
-        {
-            var addForm = new frmAddEditDatabase();
-            addForm.GetErrorsOnDatabaseInformation = _databasesAppData.CheckForErrorsOnName;
-
-            var dialogResult = addForm.ShowDialog();
-            var databaseList = ((ArrangeableList<INamed>)_databasesAppData.Instances);
-            if (dialogResult == DialogResult.OK)
-            {
-                var selectedIndices = lvDatabases.SelectedIndices.Cast<int>().ToList();
-                databaseList.Add(addForm.DatabaseInformation);
-                PopulateDatabasesListView(selectedIndices);
-            }
-        }
-
-        private bool IsThereAnyItemSelected(ListView listView)
-        {
-            return listView.SelectedItems.Count > 0;
+            EditSelectedItem();
         }
 
         private void menuOpenServersConfiguration_Click(object sender, EventArgs e)
@@ -203,13 +114,225 @@ namespace DbScriptRunner.UI
             if (!string.IsNullOrEmpty(fullPath))
             {
                 _databasesAppData.Load(Path.GetFileName(fullPath), Path.GetDirectoryName(fullPath));
-                PopulateDatabasesListView();
+                PopulateListView(_databasesAppData.Instances, lvDatabases);
             }
         }
 
         private void menuSaveServersConfiguration_Click(object sender, EventArgs e)
         {
             SaveDatabases();
+        }
+
+        private void addServerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddNewDatabaseItem(lvDatabases);
+        }
+
+        private void editSelectedServerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EditSelectedDatabaseItem(lvDatabases);
+        }
+
+        private void menuOpenScriptConfiguration_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not implemented!");
+        }
+
+        private ListView GetFocusedListView()
+        {
+            if (lvDatabases.Focused) return lvDatabases;
+            if (lvScripts.Focused) return lvScripts;
+
+            CommonDialogs.TellUserToFocusAListView();
+
+            return null;
+        }
+
+        private void PopulateListView(IArrangeableList<INamed> datasource, ListView listView, List<int> indicesToSelect = null, string lvTitle = "")
+        {
+            listView.Items.Clear();
+
+            if (datasource == null || !datasource.Any()) return;
+
+            listView.SuspendLayout();
+            listView.Tag = datasource;
+            int index = 0;
+            foreach (var item in datasource)
+            {
+                ListViewItem lvItem = listView.Items.Add(index.ToString());
+                lvItem.Tag = item;
+                lvItem.SubItems.Add(item.Name);
+                ++index;
+            }
+
+            SelectIndicesOnListView(listView, indicesToSelect);
+
+            if (!string.IsNullOrEmpty(lvTitle)) listView.Text = lvTitle.ToUpper();
+
+            listView.ResumeLayout();
+        }
+
+        private void SelectIndicesOnListView(ListView lvControl, List<int> indicesToSelect)
+        {
+            if (indicesToSelect == null) return;
+
+            var maxIndex = lvControl.Items.Count;
+            foreach (var index in indicesToSelect)
+                lvControl.SelectedIndices.Add(index);
+        }
+
+        private void RemoveItem()
+        {
+            var focusedListView = GetFocusedListView();
+            if (focusedListView == null) return;
+
+            if (!IsThereAnyItemSelected(focusedListView))
+            {
+                CommonDialogs.TellUserToSelectItemsInOrderToMove();
+                return;
+            }
+
+            var selectedIndices = focusedListView.SelectedIndices.Cast<int>().ToList();
+            var datasource = (ArrangeableList<INamed>)focusedListView.Tag;
+            var indicesToSelect = datasource.RemoveAt(selectedIndices);
+            PopulateListView(datasource, focusedListView, indicesToSelect);
+        }
+
+        private void MoveItemDown()
+        {
+            ListView focusedListView = GetFocusedListView();
+            if (focusedListView == null) return;
+
+            if (!IsThereAnyItemSelected(focusedListView))
+            {
+                CommonDialogs.TellUserToSelectItemsInOrderToMove();
+                return;
+            }
+
+            var selectedIndices = focusedListView.SelectedIndices.Cast<int>().ToList();
+            var datasource = (ArrangeableList<INamed>)focusedListView.Tag;
+            var indicesToSelect = datasource.MoveItemsDownOnePosition(selectedIndices);
+            PopulateListView(datasource, focusedListView, indicesToSelect);
+        }
+
+        private void MoveItemUp()
+        {
+            ListView focusedListView = GetFocusedListView();
+            if (focusedListView == null) return;
+
+            if (!IsThereAnyItemSelected(focusedListView))
+            {
+                CommonDialogs.TellUserToSelectItemsInOrderToMove();
+                return;
+            }
+
+            var selectedIndices = focusedListView.SelectedIndices.Cast<int>().ToList();
+            var datasource = (ArrangeableList<INamed>)focusedListView.Tag;
+            var indicesToSelect = datasource.MoveItemsUpOnePosition(selectedIndices);
+            PopulateListView(datasource, focusedListView, indicesToSelect);
+        }
+
+        private void EditSelectedItem()
+        {
+            ListView focusedListView = GetFocusedListView();
+            if (focusedListView == null) return;
+
+            if (focusedListView.SelectedIndices.Count != 1)
+            {
+                CommonDialogs.TellUserToSelectJustOneItem();
+                return;
+            }
+
+            if (focusedListView == lvDatabases) EditSelectedDatabaseItem(focusedListView);
+            else EditSelectedScriptItem(focusedListView);
+        }
+
+        private void EditSelectedScriptItem(ListView focusedListView)
+        {
+            var datasource = (ArrangeableList<INamed>)focusedListView.Tag;
+
+            var editForm = new frmAddEditScript();
+            editForm.GetErrorsOnItemInformation = _scriptsAppData.CheckForErrorsOnName;
+            editForm.ItemInformation = (Script)focusedListView.SelectedItems[0].Tag;
+
+            var dialogResult = editForm.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                var selectedIndices = focusedListView.SelectedIndices.Cast<int>().ToList();
+                datasource.ReplaceAt(focusedListView.SelectedIndices[0], editForm.ItemInformation);
+                PopulateListView(datasource, focusedListView, selectedIndices);
+            }
+        }
+
+        private void EditSelectedDatabaseItem(ListView focusedListView)
+        {
+            var datasource = (ArrangeableList<INamed>)focusedListView.Tag;
+
+            var editForm = new frmAddEditDatabase();
+            editForm.GetErrorsOnDatabaseInformation = _databasesAppData.CheckForErrorsOnName;
+            editForm.DatabaseInformation = (Database)focusedListView.SelectedItems[0].Tag;
+
+            var dialogResult = editForm.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                var selectedIndices = focusedListView.SelectedIndices.Cast<int>().ToList();
+                datasource.ReplaceAt(focusedListView.SelectedIndices[0], editForm.DatabaseInformation);
+                PopulateListView(datasource, focusedListView, selectedIndices);
+            }
+        }
+
+        private void AddNewItem()
+        {
+            ListView focusedListView = GetFocusedListView();
+            if (focusedListView == null) return;
+
+            if (focusedListView.SelectedIndices.Count != 1)
+            {
+                CommonDialogs.TellUserToSelectJustOneItem();
+                return;
+            }
+
+            if (focusedListView == lvDatabases) AddNewDatabaseItem(focusedListView);
+            else AddNewScriptItem(focusedListView);
+        }
+
+        private void AddNewDatabaseItem(ListView focusedListView)
+        {
+            var datasource = (ArrangeableList<INamed>)focusedListView.Tag;
+
+            var addForm = new frmAddEditDatabase();
+            addForm.GetErrorsOnDatabaseInformation = _databasesAppData.CheckForErrorsOnName;
+
+            var dialogResult = addForm.ShowDialog();
+            var databaseList = ((ArrangeableList<INamed>)_databasesAppData.Instances);
+            if (dialogResult == DialogResult.OK)
+            {
+                var selectedIndices = focusedListView.SelectedIndices.Cast<int>().ToList();
+                databaseList.Add(addForm.DatabaseInformation);
+                PopulateListView(datasource, focusedListView, selectedIndices);
+            }
+        }
+
+        private void AddNewScriptItem(ListView focusedListView)
+        {
+            var datasource = (ArrangeableList<INamed>)focusedListView.Tag;
+
+            var addForm = new frmAddEditScript();
+            addForm.GetErrorsOnItemInformation = _scriptsAppData.CheckForErrorsOnName;
+
+            var dialogResult = addForm.ShowDialog();
+            var databaseList = ((ArrangeableList<INamed>)_databasesAppData.Instances);
+            if (dialogResult == DialogResult.OK)
+            {
+                var selectedIndices = focusedListView.SelectedIndices.Cast<int>().ToList();
+                databaseList.Add(addForm.ItemInformation);
+                PopulateListView(datasource, focusedListView, selectedIndices);
+            }
+        }
+
+        private bool IsThereAnyItemSelected(ListView listView)
+        {
+            return listView.SelectedItems.Count > 0;
         }
 
         private void SaveDatabases()
@@ -223,19 +346,48 @@ namespace DbScriptRunner.UI
             }
         }
 
-        private void addServerToolStripMenuItem_Click(object sender, EventArgs e)
+        private void SaveScripts()
         {
-            AddNewDatabase();
+            throw new NotImplementedException();
         }
 
-        private void editSelectedServerToolStripMenuItem_Click(object sender, EventArgs e)
+        private void CheckScriptConfigIsSaved(FormClosingEventArgs e)
         {
-            EditSelectedDatabaseItem();
+            DialogResult dialogResult = DialogResult.OK;
+            if (_scriptsAppData.HaveChanged())
+            {
+                dialogResult = MessageBox.Show("Script configuration has changed. Want to save the current list?", "CONTENT HAVE CHANGED", MessageBoxButtons.YesNoCancel);
+                if (dialogResult == DialogResult.Yes)
+                    SaveScripts();
+            }
+
+            if (dialogResult == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+                return;
+            }
+            else
+                _scriptsAppData.BackupCurrentStatus();
         }
 
-        private void menuOpenScriptConfiguration_Click(object sender, EventArgs e)
+        private void CheckDatabaseConfigIsSaved(FormClosingEventArgs e)
         {
+            DialogResult dialogResult = DialogResult.OK;
+            if (_databasesAppData.HaveChanged())
+            {
+                dialogResult = MessageBox.Show("Databases changed. Want to save the current list?", "CONTENT HAVE CHANGED", MessageBoxButtons.YesNoCancel);
+                if (dialogResult == DialogResult.Yes)
+                    SaveDatabases();
+            }
 
+            if (dialogResult == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+                return;
+            }
+            else
+                _databasesAppData.BackupCurrentStatus();
         }
+
     }
 }
